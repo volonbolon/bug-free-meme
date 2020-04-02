3fc78b4dec7f2b749a4ceaa60f194b2a11fa0d93
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-02 16:31:20 -0300

    Add BugFreeMemeUIKit

---
+++ BugFreeMeme/BugFreeMemeUIKit/BugFreeMemeUIKit.h
@@ -0,0 +1,19 @@
+//
+//  BugFreeMemeUIKit.h
+//  BugFreeMemeUIKit
+//
+//  Created by Ariel Rodriguez on 02/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BugFreeMemeUIKit.
+FOUNDATION_EXPORT double BugFreeMemeUIKitVersionNumber;
+
+//! Project version string for BugFreeMemeUIKit.
+FOUNDATION_EXPORT const unsigned char BugFreeMemeUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugFreeMemeUIKit/PublicHeader.h>
+
+
