5c3611f588197488b3128e73408a0bf65d4b51d3
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-02 16:35:22 -0300

    Add nibless components

---
+++ BugFreeMeme/BugFreeMemeUIKit/Nibless Components/NiblessTableView.swift
@@ -0,0 +1,22 @@
+//
+//  NiblessTableView.swift
+//  BugFreeMemeUIKit
+//
+//  Created by Ariel Rodriguez on 02/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class NiblessTableView: UITableView {
+    public override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+    }
+
+    @available(*, unavailable,
+    message: "Loading this table view from a nib is unsupported in favor of initializer dependency injection."
+    )
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("Loading this table view from a nib is unsupported in favor of initializer dependency injection.")
+    }
+}
---
+++ BugFreeMeme/BugFreeMemeUIKit/Nibless Components/NiblessView.swift
@@ -0,0 +1,22 @@
+//
+//  NiblessView.swift
+//  BugFreeMemeUIKit
+//
+//  Created by Ariel Rodriguez on 02/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class NiblessView: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable,
+    message: "Loading this view from a nib is unsupported in favor of initializer dependency injection."
+    )
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("Loading this view from a nib is unsupported in favor of initializer dependency injection.")
+    }
+}
