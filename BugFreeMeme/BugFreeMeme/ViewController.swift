2c660eda46dd710108b79172d3db19d6a5a518a7
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-02 16:30:23 -0300

    Setup project

---
+++ BugFreeMeme/BugFreeMeme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BugFreeMeme
+//
+//  Created by Ariel Rodriguez on 02/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
