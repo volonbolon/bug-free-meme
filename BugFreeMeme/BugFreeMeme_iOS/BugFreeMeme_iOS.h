cad66ffe4d9a4339b650f8cda22f731a7e37a9eb
Ariel Rodriguez <ariel@volonbolon.me>
2020-04-02 16:36:10 -0300

    Add BugFreeMeme_iOS

---
+++ BugFreeMeme/BugFreeMeme_iOS/BugFreeMeme_iOS.h
@@ -0,0 +1,19 @@
+//
+//  BugFreeMeme_iOS.h
+//  BugFreeMeme_iOS
+//
+//  Created by Ariel Rodriguez on 02/04/2020.
+//  Copyright © 2020 Ariel Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BugFreeMeme_iOS.
+FOUNDATION_EXPORT double BugFreeMeme_iOSVersionNumber;
+
+//! Project version string for BugFreeMeme_iOS.
+FOUNDATION_EXPORT const unsigned char BugFreeMeme_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugFreeMeme_iOS/PublicHeader.h>
+
+
